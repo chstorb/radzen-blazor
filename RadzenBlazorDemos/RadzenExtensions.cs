63577d1230ab95b74690441ca322295e3e43b720
Christian Storb <c.storb@wibutler.io>
2023-06-09 11:18:20 +0200

    no way to change  context menu items based on target data

---
+++ RadzenBlazorDemos/RadzenExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using System.Collections.Generic;
+
+namespace RadzenBlazorDemos;
+
+static class RadzenExtensions
+{
+    public static void OpenContextMenu(this ContextMenuService src, MouseEventArgs args, IEnumerable<ContextMenuItem> items)
+    {
+        src.Open(args, items, menuItemEventArgs =>
+        {
+            EventCallback<MenuItemEventArgs> callback = (EventCallback<MenuItemEventArgs>)menuItemEventArgs.Value;
+            callback.InvokeAsync();
+        });
+    }
+}
